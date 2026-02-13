f64f420fa7c958eea5396a2eb8d2b7486f991a11
glMuller <glmuller2001@gmail.com>
2026-02-12 22:52:41 -0300

    exercicio 2.1 - Dando um novo valor a uma variável ja existente.

---
+++ aula120226/exercicio2-1.js
@@ -0,0 +1,9 @@
+const nomeJogador = "Ana"
+
+let pontos = 0
+
+console.log("Os pontos de: " + nomeJogador + " são: " + pontos)
+
+pontos = 10
+
+console.log("Os pontos de: " + nomeJogador + " são: " + pontos)
