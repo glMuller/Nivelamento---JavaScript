6d1f4e17488691537f97fc2f73cd3f52b1dc7afa
glMuller <glmuller2001@gmail.com>
2026-02-12 22:09:51 -0300

    Exercicio 1.1 resolvido

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Meu nome é Gabriel, e estou no terceiro semestre de ADS na Fiap.")
+console.log("Curso ADS porque gosto de desenvolver e de tech!")
+console.log("Estou aprendendo JavaScript")
