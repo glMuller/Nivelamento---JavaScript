c8b730637db9c7f2df919c61270b48afbee6dca5
glMuller <glmuller2001@gmail.com>
2026-02-14 11:25:23 -0300

    exercicio 4.1 - usando arrays, push e length

---
+++ aula120226/4.1.js
@@ -0,0 +1,9 @@
+let tarefas = ["Fazer café" , "Programar" , "Correr"];
+console.log(tarefas[0]);
+console.log(tarefas[2]);
+console.log(tarefas.length);
+
+tarefas.push("Comprar pão");
+
+console.log(tarefas.length);
+console.log(tarefas[3]);
