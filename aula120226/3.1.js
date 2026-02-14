5fcb02bf8cc5856e42d417f3c372a9af0c829443
glMuller <glmuller2001@gmail.com>
2026-02-14 10:47:19 -0300

    exercicio 3.1 - Diferença entre null e undefined

---
+++ aula120226/3.1.js
@@ -0,0 +1,11 @@
+let enderecoEntrega = null;
+let numeroPedidos;
+
+console.log(enderecoEntrega)
+console.log(numeroPedidos)
+
+enderecoEntrega = "Rua dos coqueiros, 123"
+numeroPedidos = 43
+
+console.log(enderecoEntrega)
+console.log(numeroPedidos)
