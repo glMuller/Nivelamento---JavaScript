1dc7d92ae134630fc40a8dc9c6353cbb43e1a992
glMuller <glmuller2001@gmail.com>
2026-02-14 10:32:12 -0300

    exercicio 2.2 - Variaveis e seus tipos

---
+++ aula120226/2.2.js
@@ -0,0 +1,9 @@
+let nome = "Gabriel Müller"
+let idade = 19
+const corinthians = true
+let cantorFavorito = undefined
+
+console.log("Nome: " , nome , " || Tipo de dado: ",typeof nome)
+console.log("Idade: ", idade, " || tipo de dado: ",typeof idade)
+console.log("Nome: ", corinthians, " || Tipo de dado: ",typeof corinthians)
+console.log("Cantor favorito:",cantorFavorito," || Tipo de dado: ",typeof cantorFavorito)
