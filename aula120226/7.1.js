2c109922049aa89c567c3468b55f795a48e718bb
glMuller <glmuller2001@gmail.com>
2026-02-18 20:58:57 -0300

    Exercicio 7.1 - Usando funções da forma padrão e com ArrowFunction, mas chegando no mesmo resultado.

---
+++ aula120226/7.1.js
@@ -0,0 +1,7 @@
+function calcularArea (base, altura){
+    return "Área do Triângulo: " + base * altura / 2
+}
+console.log(calcularArea(4,5))
+
+const calcularAreaArrow = (base, altura) => "Área do triângulo: " + base * altura / 2;
+console.log(calcularAreaArrow(4,5))
